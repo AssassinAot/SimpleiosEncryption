2edd07a3b296e0c2d43350c624d21a10866f911c
deathsky0725 <wbenjapol@gmail.com>
2018-07-10 16:02:18 +0700

    Initial Commit

---
+++ SimpleiosEncryption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleiosEncryption
+//
+//  Created by Benjapol Suksakulchai on 10/7/2561 BE.
+//  Copyright © 2561 Benjapol Suksakulchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
